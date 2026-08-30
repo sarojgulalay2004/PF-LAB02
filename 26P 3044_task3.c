9dc331f7be0325256d9bc015562b548060202d85
sarojgulalay2004 <sarojgulalay@gmail.com>
2026-08-30 00:17:09 -0700

    Add files via upload

---
+++ 26P 3044_task3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+  printf("\tC ESCAPE SEQUENCES REFERENCE\n1.  To print a double quote, write:\t\\\"\n2.  To print a literal backslash, write:\t\\\\\n3.  To insert a new line break, write:\t\\n\n4.  To insert a horizontal tab stop, write:\t\\t\n5.  To print a percent sign, write:\t%%%%");
+
+return 0;
+ 
+}
