28a72380eb8b86ce17b2272b55751ba31e4147a9
sarojgulalay2004 <sarojgulalay@gmail.com>
2026-08-30 00:20:36 -0700

    pf lab task

---
+++ 26p 3044_task5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+float c = 37.50;
+float f = 99.50;
+float k = 310.65;
+
+  printf("\tTEMPERATURE CONVERSION\nTemperature in Celsius:\t\t%.2f C\nTemperature in Farenheit:\t%.2f F\nTemperature in Kelvin:\t\t%.2f K", c,f,k);
+
+retuen 0;
+
+}
