e77bc32a67d4b5e5587ff7a258f82373a633e6f0
sarojgulalay2004 <sarojgulalay@gmail.com>
2026-08-30 00:16:38 -0700

    pf lab task 2

---
+++ 26p3044_task2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int batch = 2026;
+      float fast_score= 62.8;
+      char sec = 'B';
+      double fee = 250000.89;
+
+   
+
+  printf("\tFAST-NUCES STUDENT PROFILE CARD\n BATCH YEAR:\t%d\n SECTION:\t%c\n TEST SCORE:\t%.1f\n SEMESTER FEE:\t%.2lf\n MOTTO:\t\t\"Small daily steps lead to big success\"",batch,fast_score,sec,fee);
+
+    return 0;
+}
